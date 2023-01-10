e6b3986c7c68338da52359a967378098687f32b0
Vic Valenzuela <victor@gangverk.is>
2023-01-10 09:39:59 +0000

    Adds tmux config, updates install script

--- install_osx.sh
+++ install_osx.sh
@@ -9,6 +9,10 @@ brew bundle
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install tmux plugins 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+
 # stow dotfiles
 stow config
 stow git
