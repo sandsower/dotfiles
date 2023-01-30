2df886af620e676faf577e60fcdba35ab9107400
Vic Valenzuela <victor@gangverk.is>
2023-01-30 21:13:06 +0000

    Enables treesitter support, adds more files to tsserver lsp

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -3,7 +3,7 @@ require "plugins"
 require "lsp"
 require "users.keymaps"
 require "users.nvim-tree"
--- require "users.nvim-treesitter"
+require "users.nvim-treesitter"
 require "users.cmp"
 require "users.refactoring"
 require "users.galaxyline"
@@ -18,4 +18,5 @@ require "users.golang"
 require "which-key".setup() 
 require "hop".setup()
 -- require "telescope".load_extension('fzf')
+
 
