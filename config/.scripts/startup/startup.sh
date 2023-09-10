4f4a64a67d74727278a25d7f7a508b28f3ae1d45
Vic Valenzuela <victor@gangverk.is>
2023-09-10 17:18:41 +0000

    Adds polybar support, various plugins and fixes

---
+++ config/.scripts/startup/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# Core components (order is important!)
+(
+    feh --bg-fill ~/.wallpapers/Space_Spiral_Nord.png &
+    (
+        picom -b 
+        ~/xborder/xborders -c ~/.config/picom/xborder.json
+    ) &
+    ~/.config/polybar/launch.sh
+) &
+
+# Services
+dbus-launch dunst --config ~/.config/dunst/dunstrc &
